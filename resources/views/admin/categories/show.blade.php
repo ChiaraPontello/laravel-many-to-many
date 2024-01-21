ec389dd01846c0687baf1e5f936ee141ea1a4124
ChiaraPontello <chiarapontello1@gmail.com>
2024-01-21 15:17:42 +0100

    modifiche show.blade categoy + project

--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -2,9 +2,12 @@
 @section('content')
     <section class="container">
         <h1>{{$category->name}}</h1>
-        <ul>
+        <h3>Project List</h3>
+        <ul class="list-group list-group-flush">
             @forelse ($category->projects as $project )
-                <li>{{$project->title}}</li>
+                <li class="list-group-item">
+                    <a href="{{route('admin.projects.show', $project->slug)}}" class="link-underline"></a>
+                </li>
                 @empty
                 <li>No project</li>
             @endforelse
--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -1,9 +1,21 @@
 @extends('layouts.app')
 @section('content')
-    <section class="container">
-        <h1>{{$project->title}}</h1>
-        <p>{{$project->body}}></p>
-        <span> {{$project->category ? $project->category->name : 'Uncategorized'}}</span>
+    <section class="container my-3" id="item-post">
+        <div class="d-flex justify-content-between align-item-center">
+            <h1>{{$project->title}}</h1>
+            <a href="{{route('admin.projects.edit', $project->slug)}}" class="btn btn-success px-3">Edit</a>
+        </div>
+        <div>
+        <p>{{!! $project->body !!}}></p>
+        @if ($project->category)
+            <div class="mb-3">
+                <h4>Category</h4>
+                <a href="{{route('admin.categories.show', $project->category->slug)}}">{{$project->category->name : 'Uncategorized'}}</a>
+            </div>
+        @endif
+        <img src="{{asset('storage/' . $project->image)}}" alt="{{$project->title}}">
+
+        </div>
     </section>
 
 @endsection
