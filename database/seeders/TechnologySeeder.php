8235e10ef9f7cf3b9cf9b095e1e70eff6804e7db
ChiaraPontello <chiarapontello1@gmail.com>
2024-01-21 16:14:05 +0100

    add TechnologySeeder + artisan db:seed

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Technology;
+use Illuminate\Support\Str;
 
 class TechnologySeeder extends Seeder
 {
@@ -12,6 +14,12 @@ class TechnologySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $technologies = ['in viaggio', 'capelli', 'Elon Musk'];
+        foreach ($technologies as $technology) {
+            $newTechnology = new Technology();
+            $newTechnology->name = $technology;
+            $newTechnology->slug = Str::slug($technology, '-');
+            $newTechnology->save();
+        }
     }
 }
